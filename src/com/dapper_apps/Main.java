9ea34996f9adecea3e4dd5094f2002804c56e074
Francisco Aguilera <Falven2000@Hotmail.com>
2016-04-20 18:59:24 -0600

    Adding metacode.

--- src/com/dapper_apps/Main.java
+++ src/com/dapper_apps/Main.java
@@ -1,10 +1,18 @@
 package com.dapper_apps;
 
+import java.util.Scanner;
+
 /**
  * Created by falven on 4/20/16.
  */
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int t = input.nextInt();
+        String[] w = input.nextLine().trim().split("\\s+");
+        for(int i = input.nextInt(); i > 0; ++i) {
+
+        }
     }
 }
